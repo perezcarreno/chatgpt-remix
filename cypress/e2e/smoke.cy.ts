0830f88d1a389ada4e29e417e614a9993d7620d7
Armando J. Perez-Carreno <info@perezcarreno.com>
2023-04-08 19:43:54 -0500

    Updated tests

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -1,20 +1,14 @@
 import { faker } from "@faker-js/faker";
 
 describe("smoke tests", () => {
-  afterEach(() => {
-    cy.cleanupUser();
-  });
-
   it("should allow you to register and login", () => {
     const loginForm = {
       email: `${faker.internet.userName()}@example.com`,
       password: faker.internet.password(),
     };
-
     cy.then(() => ({ email: loginForm.email })).as("user");
 
     cy.visitAndCheck("/");
-
     cy.findByRole("link", { name: /sign up/i }).click();
 
     cy.findByRole("textbox", { name: /email/i }).type(loginForm.email);
@@ -26,12 +20,11 @@ describe("smoke tests", () => {
     cy.findByRole("link", { name: /log in/i });
   });
 
-  it("should allow you to make a conversation", () => {
+  it("should allow you to create a conversation", () => {
     const testConversation = {
       title: faker.lorem.words(1),
     };
     cy.login();
-
     cy.visitAndCheck("/");
 
     cy.findByRole("link", { name: /conversations/i }).click();
@@ -40,7 +33,7 @@ describe("smoke tests", () => {
     cy.findByRole("link", { name: /\+ new conversation/i }).click();
 
     cy.findByRole("textbox", { name: /title/i }).type(testConversation.title);
-    cy.findByRole("button", { name: /save/i }).click();
+    cy.findByRole("button", { name: /create conversation/i }).click();
 
     cy.findByRole("button", { name: /delete/i }).click();
 
