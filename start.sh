384eaf675c4e47c66aaa73d7cc56e7b4e5d94ed2
Armando J. Perez-Carreno <info@perezcarreno.com>
2023-04-08 22:48:28 -0500

    Added swap for Fly.io

--- start.sh
+++ start.sh
@@ -6,5 +6,20 @@
 # Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
 
 set -ex
+
+# Setup 512MB of space for swap and set permissions and turn on swapmode
+fallocate -l 512M /swapfile
+chmod 0600 /swapfile
+mkswap /swapfile
+echo 10 > /proc/sys/vm/swappiness
+swapon /swapfile
+
+# Run migrations
 npx prisma migrate deploy
+
+# Turn off swap mode and remove swap directory
+swapoff /swapfile
+rm /swapfile
+
+# Finally start the app
 npm run start
